5793ceb397ef2a2b86678d9a2e5a0e9b665616d5
Cesar Salazar Silva <cesar.salazar@usach.cl>
2020-08-12 17:51:25 -0400

    migraciones-faltan pk y fk

---
+++ database/migrations/2020_08_12_204558_create_assessments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssessmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assessments', function (Blueprint $table) {
+            $table->id();
+            $table->smallInteger('score');
+            $table->date('date');
+            $table->string('comment', "500");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assessments');
+    }
+}
